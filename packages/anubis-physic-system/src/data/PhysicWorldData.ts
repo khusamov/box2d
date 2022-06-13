d37aa4818b46fac25390f6e4f0214aa1e91c4747
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-13 16:37:13 +0300

    Параметры физического мира можно не определять (есть значения по умолчанию для всех параметров)

--- packages/anubis-physic-system/src/data/PhysicWorldData.ts
+++ packages/anubis-physic-system/src/data/PhysicWorldData.ts
@@ -4,7 +4,7 @@ import {IWorldDef} from '../interfaces/IWorldDef'
 
 export class PhysicWorldData extends Data {
 	constructor(
-		public readonly worldDef: IWorldDef,
+		public readonly worldDef?: IWorldDef,
 		public readonly world?: World
 	) {
 		super()
