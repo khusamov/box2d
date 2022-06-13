ded0f6317c137f0aa444251ee8fef411e584b009
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-13 16:43:22 +0300

    В интерфейс IEventEmitter добавлен setMaxListeners

--- packages/anubis-message-broker/src/interfaces/IEventEmitter.ts
+++ packages/anubis-message-broker/src/interfaces/IEventEmitter.ts
@@ -5,4 +5,6 @@ export interface IEventEmitter {
 	once(type: string | number, listener: Listener): this
 	emit(type: string | number, ...args: any[]): boolean
 	off(type: string | number, listener: Listener): this
+	setMaxListeners(n: number): this
+	getMaxListeners(): number
 }
