b867cf3d1aef50accd8dcb780d1e8d3e99dce3a4
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-14 17:34:57 +0300

    Класс FakeMessageEmitter перенесен в anubis-message-broker

--- packages/anubis-message-broker/src/classes/FakeMessageEmitter.ts
+++ packages/anubis-message-broker/src/classes/FakeMessageEmitter.ts
@@ -1,9 +1,13 @@
-import {IMessageEmitter} from 'anubis-message-broker'
 import {IDisposable} from 'base-types'
+import {IMessageEmitter} from '../interfaces/IMessageEmitter'
 
 const replaceMessageError = new Error('Замените FakeMessageEmitter на MessageEmitter')
 
 export class FakeMessageEmitter implements IMessageEmitter {
+    dispose(): void {
+        throw replaceMessageError
+    }
+
 	emit(): void {
 		throw replaceMessageError
 	}
