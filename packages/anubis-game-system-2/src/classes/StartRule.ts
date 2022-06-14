4549f278bd46f6623ee023de43841fdd5943c847
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-14 17:37:20 +0300

    Добавлены комментарии

--- packages/anubis-game-system-2/src/classes/StartRule.ts
+++ packages/anubis-game-system-2/src/classes/StartRule.ts
@@ -2,6 +2,10 @@ import {Rule} from 'anubis-rule-system'
 import {UpdateMessage} from '../messages/UpdateMessage'
 import {StartMessage} from '../messages/StartMessage'
 
+/**
+ * Правило, которое срабатывает один раз в начале игры.
+ * Предназначено для создания сущностей перед игрой.
+ */
 export abstract class StartRule extends Rule {
 	public override init() {
 		this.messageEmitter.once(UpdateMessage, ({dataStorage}) => {
--- packages/anubis-game-system-2/src/messages/StartMessage.ts
+++ packages/anubis-game-system-2/src/messages/StartMessage.ts
@@ -1,6 +1,9 @@
 import {Message} from 'anubis-message-broker'
 import {IDataStorage} from 'anubis-data-storage'
 
+/**
+ * Используется в StartRule.
+ */
 export class StartMessage extends Message {
 	public constructor(
 		public readonly dataStorage: IDataStorage
