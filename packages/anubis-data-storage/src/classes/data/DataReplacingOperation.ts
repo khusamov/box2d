859fdec644486c5d9e8bf36fcf703a564e20051c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-14 17:34:11 +0300

    Оказывается не стоит генерировать события добавления и удаления при замене данных

--- packages/anubis-data-storage/src/classes/data/DataReplacingOperation.ts
+++ packages/anubis-data-storage/src/classes/data/DataReplacingOperation.ts
@@ -1,19 +1,25 @@
 import {IMessageEmitter} from 'anubis-message-broker'
 import {IEntity} from '../../interfaces/IEntity'
 import {IData} from '../../interfaces/IData'
-import {DataAddingOperation} from './DataAddingOperation'
-import {DataDeletingOperation} from './DataDeletingOperation'
 import {DataReplacingMessage} from '../../messages/DataReplacingMessage'
+import {setParent} from '../../functions/setParent'
 
+/**
+ * @event DataReplacingMessage
+ */
 export class DataReplacingOperation {
 	public constructor(
 		private readonly messageEmitter: IMessageEmitter,
 		private readonly parentEntity: IEntity
 	) {}
 
-	public replace(previousData: IData, data: IData) {
-		new DataDeletingOperation(this.messageEmitter, this.parentEntity).delete(previousData)
-		new DataAddingOperation(this.messageEmitter, this.parentEntity).add(data)
+	public replace<D extends IData>(previousData: D, data: D) {
+		// Удалить предыдущие данные.
+		this.parentEntity.splice(this.parentEntity.indexOf(previousData), 1)
+		// Добавить новые данные.
+		setParent(this.parentEntity, data)
+		this.parentEntity.push(data)
+		// Отправить сообщение о замене данных.
 		this.messageEmitter.emit(new DataReplacingMessage(previousData, data))
 	}
 }
