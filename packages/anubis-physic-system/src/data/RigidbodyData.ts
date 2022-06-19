1f79bcab8c575818ebf51226042af7e5edc0e1a3
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-19 18:25:40 +0300

    Исправлено правило RigidbodyDeletionRule

--- packages/anubis-physic-system/src/data/RigidbodyData.ts
+++ packages/anubis-physic-system/src/data/RigidbodyData.ts
@@ -1,12 +1,15 @@
 import {Body} from 'planck'
 import {IBodyDef} from '../interfaces/IBodyDef'
 import {Data} from 'anubis-data-storage'
+import {externalDataSymbol, IExternalData} from 'anubis-deletion-system'
 
 /**
  * Данные для создания твердого тела.
  * Могут содержаться только в корне сущности, для которой создается твердое тело.
  */
-export class RigidbodyData extends Data {
+export class RigidbodyData extends Data implements IExternalData {
+	public readonly [externalDataSymbol] = true
+
 	public constructor(
 		public readonly bodyDef: IBodyDef,
 		public readonly body?: Body
--- packages/anubis-physic-system/src/rules/RigidbodyDeletionRule.ts
+++ packages/anubis-physic-system/src/rules/RigidbodyDeletionRule.ts
@@ -1,26 +1,14 @@
-import {Rule} from 'anubis-rule-system'
-import {DataAfterAddingMessage, DataStorageFasade, isData} from 'anubis-data-storage'
-import {DeletedMarkData, UpdateMessage} from 'anubis-game-system-2'
+import {DeletionRule} from 'anubis-deletion-system'
+import {DataStorage, DataStorageFasade, IEntity, isData} from 'anubis-data-storage'
 import {RigidbodyData} from '../data/RigidbodyData'
 import {PhysicWorldData} from '../data/PhysicWorldData'
 
-export class RigidbodyDeletionRule extends Rule {
-	public init() {
-		this.messageEmitter.on(DataAfterAddingMessage, ({data}) => {
-			if (data instanceof DeletedMarkData) {
-				this.messageEmitter.once(UpdateMessage, ({dataStorage}) => {
-					const deletedEntity = new DataStorageFasade(dataStorage).createDataFasade(data).entity
-					const body = deletedEntity.find(isData(RigidbodyData))?.body
-					const world = new DataStorageFasade(dataStorage).find(isData(PhysicWorldData))?.world
-					if (body && world) {
-						world.destroyBody(body)
-
-						// TODO Сделать удаление только данных Rigidbody, а удаление самой сущности
-						//  должно выполнять правило, которое отслеживает сущности с Deleted, но без данных.
-						new DataStorageFasade(dataStorage).deleteEntity(deletedEntity)
-					}
-				})
-			}
-		})
+export class RigidbodyDeletionRule extends DeletionRule {
+	protected deletion(deletedEntity: IEntity, dataStorage: DataStorage) {
+		const body = deletedEntity.find(isData(RigidbodyData))?.body
+		const world = new DataStorageFasade(dataStorage).find(isData(PhysicWorldData))?.world
+		if (body && world) {
+			world.destroyBody(body)
+		}
 	}
 }
