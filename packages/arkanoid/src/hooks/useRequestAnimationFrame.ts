e46e25bafefb8908566952e1be58f294dac7e8a0
Khusamov Sukhrob <khusamov@yandex.ru>
2022-06-27 19:11:19 +0300

    Исправлена работа useRequestAnimationFrame()

--- packages/arkanoid/src/hooks/useRequestAnimationFrame.ts
+++ packages/arkanoid/src/hooks/useRequestAnimationFrame.ts
@@ -1,5 +1,6 @@
 import {useEffect, useRef, useState} from 'react';
 
+const emptyFunction = () => {}
 const fps = 60
 const step = 1 / fps
 
@@ -14,21 +15,24 @@ export function useRequestAnimationFrame(update: (step: number) => void): [numbe
 	const frame = useRef(0)
 	const last = useRef(performance.now())
 	const dt = useRef(0)
+	const render = useRef(emptyFunction)
 
-	const render = () => {
-		const now = performance.now();
-		dt.current = dt.current + (now - last.current) / 1000
-		while (dt.current > step) {
-			dt.current = dt.current - step
-			update(step)
+	render.current = (
+		() => {
+			const now = performance.now();
+			dt.current = dt.current + (now - last.current) / 1000
+			while (dt.current > step) {
+				dt.current = dt.current - step
+				update(step)
+			}
+			setTime(now)
+			last.current = now
+			frame.current = requestAnimationFrame(render.current)
 		}
-		setTime(now)
-		last.current = now
-		frame.current = requestAnimationFrame(render)
-	}
+	)
 
 	useEffect(() => {
-		frame.current = requestAnimationFrame(render)
+		frame.current = requestAnimationFrame(render.current)
 		return () => cancelAnimationFrame(frame.current)
 	}, [])
 
