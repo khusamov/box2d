7f11386b30ef35a4a8e0aa6cfbf2fb4b570a1b18
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-05 13:21:19 +0300

    Добавлено правило MouseJointDeletionRule

--- packages/anubis-physic-system/src/data/joint/MouseJointData.ts
+++ packages/anubis-physic-system/src/data/joint/MouseJointData.ts
@@ -2,11 +2,14 @@ import {Data} from 'anubis-data-storage'
 import {MouseJoint} from 'planck'
 import {IMouseJointDef} from '../../interfaces/IMouseJointDef'
 import {IJointFindBody} from '../../interfaces/IJointFindBody'
+import {externalDataSymbol, IExternalData} from 'anubis-deletion-system'
 
 /**
  * @link https://github.com/shakiba/planck.js/wiki/Joint#mouse-joint
  */
-export class MouseJointData extends Data {
+export class MouseJointData extends Data implements IExternalData {
+	public readonly [externalDataSymbol] = true
+
 	public constructor(
 		public readonly mouseJointDef: IMouseJointDef & IJointFindBody,
 		public readonly mouseJoint?: MouseJoint
--- packages/anubis-physic-system/src/features/JointFeature.ts
+++ packages/anubis-physic-system/src/features/JointFeature.ts
@@ -1,10 +1,12 @@
 import {Feature} from 'anubis-rule-system'
 import {MouseJointCreatorRule} from '../rules/MouseJointCreatorRule'
+import {MouseJointDeletionRule} from '../rules/MouseJointDeletionRule'
 
 export class JointFeature extends Feature {
 	public constructor() {
 		super(
-			new MouseJointCreatorRule
+			new MouseJointCreatorRule,
+			new MouseJointDeletionRule
 		)
 	}
 }
