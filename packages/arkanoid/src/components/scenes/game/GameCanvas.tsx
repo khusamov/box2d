b3c24df624a4d902eabfd21fff4bae7a1f785f6a
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-05 17:13:48 +0300

    Исправлено вычисление координат мыши

--- packages/arkanoid/src/components/scenes/game/GameCanvas.tsx
+++ packages/arkanoid/src/components/scenes/game/GameCanvas.tsx
@@ -50,7 +50,7 @@ export function GameCanvas({game}: IGameCanvasProps) {
 			game.messageEmitter.emit(
 				new BatMoveMessage(
 					movementX / scale,
-					movementY / scale
+					movementY / -scale
 				)
 			)
 		}
