eb9150bc549129bf6bcad289490df477b43a5f6a
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-05 13:39:21 +0300

    Исправлен стиль кода

--- packages/anubis-message-broker/src/interfaces/IEventEmitter.ts
+++ packages/anubis-message-broker/src/interfaces/IEventEmitter.ts
@@ -1,18 +1,12 @@
 import {EventEmitter} from 'events'
 
-export type Listener = (...args: any[]) => void
-
 type TEventEmitterPickedMethods =
-	'on' | 'once' | 'emit' | 'off' |
+	'on' |
+	'once' |
+	'off' |
+	'emit' |
 	'removeAllListeners' |
-	'setMaxListeners' | 'getMaxListeners'
+	'setMaxListeners' |
+	'getMaxListeners'
 
-export interface IEventEmitter extends Pick<EventEmitter, TEventEmitterPickedMethods> {
-	// on(type: string | number, listener: Listener): this
-	// once(type: string | number, listener: Listener): this
-	// emit(type: string | number, ...args: any[]): boolean
-	// off(type: string | number, listener: Listener): this
-	// setMaxListeners(n: number): this
-	// getMaxListeners(): number
-	// removeAllListeners(type?: string | number): this
-}
+export interface IEventEmitter extends Pick<EventEmitter, TEventEmitterPickedMethods> {}
