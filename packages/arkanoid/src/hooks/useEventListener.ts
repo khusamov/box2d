97e50a763afa2295541e2c11e8fff688c9c6a6bc
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-09 23:03:27 +0300

    Добавлен хук useEventListener()

---
+++ packages/arkanoid/src/hooks/useEventListener.ts
@@ -0,0 +1,38 @@
+import {RefObject, useEffect, useRef} from 'react'
+
+/**
+ * Специальный хук для добавления событий на элементы.
+ * Позволяет создавать обработчики со ссылками на меняющиеся данные.
+ * @param ref
+ * @param type
+ * @param options
+ * @param sourceListener
+ */
+export function useEventListener<K extends keyof HTMLElementEventMap>(
+	ref: RefObject<HTMLElement>,
+	type: K,
+	options: boolean | AddEventListenerOptions,
+	sourceListener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any
+) {
+	const sourceListenerRef = useRef(sourceListener)
+	sourceListenerRef.current = sourceListener
+
+	useEffect(() => {
+		const listener = (
+			function(this: HTMLElement, event: HTMLElementEventMap[K]) {
+				return sourceListenerRef.current.call(this, event)
+			}
+		)
+
+		if (ref.current) {
+			ref.current.addEventListener(type, listener, options)
+		}
+		return () => {
+			if (ref.current) {
+				ref.current.removeEventListener(type, listener)
+			}
+		}
+	}, [])
+}
+
+
