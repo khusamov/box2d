7d04b8a94485728f2947f763edf1f81b3f38f705
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-24 18:22:42 +0300

    Добавлена ссылка на TUserContext

--- packages/anubis-message-broker/src/interfaces/IMessageEmitter.ts
+++ packages/anubis-message-broker/src/interfaces/IMessageEmitter.ts
@@ -1,9 +1,10 @@
 import {IDisposable} from 'base-types'
 import {TMessageConstructor} from '../types/TMessageConstructor'
 import {TMessageListener} from '../types/TMessageListener'
+import {TUserContext} from '../types/TUserContext'
 import {IMessage} from './IMessage'
 
-export interface IMessageEmitter<C extends object = {}> extends IDisposable {
+export interface IMessageEmitter<C extends TUserContext = {}> extends IDisposable {
 	on<M extends IMessage>(MessageClass: TMessageConstructor<M>, listener: TMessageListener<M, C>): IDisposable
 	once<M extends IMessage>(MessageClass: TMessageConstructor<M>, listener: TMessageListener<M, C>): IDisposable
 	emit(...message: IMessage[]): void
