14e369c19979d8a74b82a59d92d38f7c886420c4
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-24 18:25:36 +0300

    Упрощен код

--- packages/anubis-game-system/src/const/suspendMessageEmitterEmit.ts
+++ packages/anubis-game-system/src/const/suspendMessageEmitterEmit.ts
@@ -1,12 +1,17 @@
 import {IMessageEmitter} from 'anubis-message-broker'
 
+const emptyFunction = () => {}
+
 /**
  * Заморозка метода IMessageEmitter.emit().
  * Необходимо для реализации функции 'Пауза в игре'.
  */
 export const suspendMessageEmitterEmit: ProxyHandler<IMessageEmitter> = {
 	get(target, property, receiver) {
-		if (property === 'emit') return () => {}
-		return Reflect.get(target, property, receiver)
+		return (
+			property === 'emit'
+				? emptyFunction
+				: Reflect.get(target, property, receiver)
+		)
 	}
 }
