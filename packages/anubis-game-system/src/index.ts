02aa9c5100a4e6d3f97c4de206a432838ebc6222
Khusamov Sukhrob <khusamov@yandex.ru>
2022-07-24 18:27:33 +0300

    С появлением IRuleContext класс StartRule выродился

--- packages/anubis-game-system/src/index.ts
+++ packages/anubis-game-system/src/index.ts
@@ -1,6 +1,5 @@
 export {Game} from './classes/Game'
 export {GameCreator} from './classes/GameCreator'
-export {StartRule} from './rules/StartRule'
 
 export {UpdateMessage} from './messages/UpdateMessage'
 
