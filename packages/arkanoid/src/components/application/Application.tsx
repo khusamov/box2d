adeec721cbc0d034ac889db8f41012c98d3219f2
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-12 16:19:21 +0300

    Состояние игры перенесено выше на один уровень

--- packages/arkanoid/src/components/application/Application.tsx
+++ packages/arkanoid/src/components/application/Application.tsx
@@ -1,7 +1,9 @@
 import {useState} from 'react'
-import {GameScene} from '../scenes/game/GameScene'
-import {SuperMario} from '../scenes/intro/SuperMario'
 import {BatMovingType, PilotLevel} from '../../game/levels/PilotLevel'
+import {useRequestAnimationFrame} from '../../hooks/useRequestAnimationFrame'
+import {GameScene} from '../scenes/game/GameScene'
+import {useGame} from '../scenes/game/useGame'
+import {SuperMario} from '../scenes/intro/SuperMario'
 
 enum Scene {
 	Intro,
@@ -10,10 +12,13 @@ enum Scene {
 
 export function Application() {
 	const [scene, setScene] = useState(Scene.Intro)
+	const level = new PilotLevel({movingType: BatMovingType.Hard})
+	const game = useGame(level)
+	useRequestAnimationFrame(timeInterval => game?.update(timeInterval))
 
 	switch (scene) {
 		case Scene.Intro: return <SuperMario onStart={() => setScene(Scene.Game)}/>
-		case Scene.Game: return <GameScene level={new PilotLevel({movingType: BatMovingType.Hard})}/>
+		case Scene.Game: return game ? <GameScene game={game}/> : null
 		default: return null
 	}
 }
--- packages/arkanoid/src/components/scenes/game/GameScene.tsx
+++ packages/arkanoid/src/components/scenes/game/GameScene.tsx
@@ -1,26 +1,20 @@
-import {Level} from 'anubis-rule-system'
+import {Game} from 'anubis-game-system'
 import {FloatPanel} from '../../panel/FloatPanel'
 import {GameCanvas} from './GameCanvas'
 import {GameSceneStyle} from './GameScene.module.scss'
 import {useScore} from './useScore'
-import {useGame} from './useGame'
-import {useRequestAnimationFrame} from '../../../hooks/useRequestAnimationFrame'
 
 interface IGameSceneProps {
-	level: Level
+	game: Game
 }
 
-export function GameScene({level}: IGameSceneProps) {
-	const game = useGame(level)
+export function GameScene({game}: IGameSceneProps) {
 	const score = useScore(game)
-	useRequestAnimationFrame(timeInterval => game?.update(timeInterval))
 
-	return !game ? null : (
+	return (
 		<div className={GameSceneStyle}>
 			<GameCanvas game={game}/>
-			<FloatPanel>
-				Счет: {score}
-			</FloatPanel>
+			<FloatPanel>Счет: {score}</FloatPanel>
 		</div>
 	)
 }
