14a90c31a96dcc13418a0cda5b995bafc26d82a7
Khusamov Sukhrob <khusamov@yandex.ru>
2022-08-12 14:05:43 +0300

    Разрешаем добавлять публичные конструкторы в абстрактные классы

--- packages/anubis-physic-system/src/data/shape/ShapeData.ts
+++ packages/anubis-physic-system/src/data/shape/ShapeData.ts
@@ -2,11 +2,9 @@ import {Shape} from 'planck'
 import {Data} from 'anubis-data-storage'
 
 export abstract class ShapeData<S extends Shape = Shape> extends Data {
-
-	// TODO Тут проблема - конструктор должен быть защищенным, но в FixtureCreatorRule он нужен публичный((
-
-	constructor(public readonly shape?: S) {
+	public constructor(public readonly shape?: S) {
 		super()
 	}
+
 	public abstract clone(shape?: S): ShapeData<S>
 }
